3a541c6ac448e0befefdbece9248f76283456e32
mktia <endoplasmicreticulum2468741059@gmail.com>
2018-07-31 23:37:17 +0900

    init

---
+++ app/src/main/java/com/mktia/www/enicy/FinishAppDialogFragment.java
@@ -0,0 +1,34 @@
+package com.mktia.www.enicy;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+
+public class FinishAppDialogFragment extends DialogFragment {
+
+    public FinishAppDialogFragment() {
+        super();
+    }
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setMessage(R.string.dialog_close_app)
+                .setPositiveButton(R.string.finish, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        getActivity().finish();
+                    }
+                })
+                .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+
+        return builder.create();
+    }
+}
